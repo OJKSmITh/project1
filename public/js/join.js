b76d9a536d78489606b6333549b828203b3c1ff2
baek <baekbr13@gmail.com>
2022-12-27 01:56:01 +0900

    feat : join페이지 html 구현

--- public/js/join.js
+++ public/js/join.js
@@ -1,17 +1,32 @@
 const formItem = document.querySelectorAll("form > div")
+const userPw = document.querySelectorAll("userPw")
+const userPwCheck = document.querySelectorAll("#userPwCheck")
+const pwTrue = document.querySelector("#pwTrue")
+const pwFalse = document.querySelector("#pwFalse")
+const items = document.querySelector("#items")
 
 for(let i = 0; i<9; i++){
-    formItem[i+1].style.display = "none";
+    formItem[0].classList ="itemAdd"
+    formItem[i+1].classList ="itemNone"
+
     const inputHandler = (e) =>{
         const target = e.target
-        console.log(e.target.value === "" || e.target.value === undefined)
         if(target.value !== "" || target.value !== undefined ){
-            formItem[i+1].style.display ="block"
+            formItem[i+1].classList="itemAdd"
+            formItem[i-1].classList="itemAdd"
+            formItem[i-1].classList="itemAdd2"
         }else{
-            formItem[i+1].style.display ="none"
+            formItem[i+1].classList ="itemNone"
+
         }
+        items.scrollTop = items.scrollHeight
     }
     formItem[i].addEventListener("input", inputHandler)
+    
+    console.log(userPw.value)
 }
-
-
+// if(formItem[1].value !== formItem[2].value){
+//     pwFalse.style.display ="block"
+// } else {
+//     pwTrue.style.display ="block"
+// }
