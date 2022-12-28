bbfb8f3d6e7dc6dfbe9df7166a269289053fa73c
Hanjin <csa0545@gamil.com>
2022-12-28 16:17:25 +0900

    feat: admin 기초 틀 완성

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,12 @@ const router = express.Router()
 const board = require("./board.route")
 const user = require("./user.route")
 const profile = require("./profile.route")
-
+const admin = require("./admin.route")
 
 
 router.use("/user", user)
-
 router.use("/board", board)
 router.use("/profile", profile)
-
+// router.user("/admin", admin)
 
 module.exports = router
