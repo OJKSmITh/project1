5b7e6a5aa63d330e290f3a97b465dd7cfd4d7cee
Hanjin <csa0545@gamil.com>
2022-12-28 16:59:07 +0900

    fix: board modify 수정

--- repository/board.repository.js
+++ repository/board.repository.js
@@ -17,7 +17,7 @@ exports.findOne = async (idx) => {
 
 exports.postModify = async (idx, subject, content, writer) => {
     const [result] = await pool.query(`UPDATE board SET subject="${subject}", content="${content}", writer="${writer}" WHERE idx=${idx} ORDER BY idx ASC`)
-    console.log(result)
+
     return result
 }
 
