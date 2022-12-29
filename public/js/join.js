7861b331a07129442d972560ef4a3e463cc93163
baek <baekbr13@gmail.com>
2022-12-29 10:03:56 +0900

    fix:css 수정

--- public/js/join.js
+++ public/js/join.js
@@ -18,13 +18,19 @@ for(let i = 0; i<10; i++){
         const target = e.target
         if(target.value !== "" || target.value !== undefined ){
             formItem[i+1].classList="itemAdd"
-            formItem[i-1].classList="itemAdd2"
+            if(i-1 >= 0){
+                formItem[i-1].classList="itemAdd2"
+            }
         }else{
             formItem[i+1].classList ="itemNone"
         }
     }
     const clickHandler =(e) =>{
         items.scrollTop = items.scrollHeight;
+        // console.dir(e.target.scrollHeight)
+        // console.dir(e.target.scrollTop)
+        
+
     }
     formItem[i].addEventListener("input", inputHandler)
     formItem[i].addEventListener("click", clickHandler)
@@ -42,9 +48,29 @@ for(let i = 0; i<10; i++){
 
 // form.addEventListener("submit", valueHandler )
 
+// let keyCodeArr1 = []
+// let keyCodeArr2 = []
 
-function keyHandler(e){
-    console.log(e.keyCode)
-}
+// function keyHandler(e){
+//     keyCodeArr1 += e.keyCode
+//     console.log(keyCodeArr1)
+// }
+// function keyHandler2(e){
+//     keyCodeArr2 += e.keyCode
+//     console.log(keyCodeArr2)
+// }
 
-password.addEventListener("keyup", keyHandler)
+// password.addEventListener("keyup", keyHandler)
+// passwordCheck.addEventListener("keyup", keyHandler2)
+
+// function checkPw(){
+//     if(keyCodeArr1 !== keyCodeArr2){
+//         password.focus()
+//         pwFalse.style.display="block"
+//     }else {
+//         pwTrue.style.display="block"
+//         history.back()
+//     }
+// }
+
+// passwordCheck.addEventListener("blur", checkPw)
--- public/js/main.js
+++ public/js/main.js
@@ -12,18 +12,20 @@ gnbBtn.addEventListener("click", gnbBtnHandler)
 
 bg_wrap.addEventListener('scroll',  function(){    //스크롤시 이벤트가 실행된다.
     let value = bg_wrap.scrollY;    //세로 스크롤 값을 value 함수에 저장한다.
-    console.log(bg_wrap.scrollTop)
-    if(bg_wrap.scrollTop < 1415){
-        bg_wrap.style.background = "url(/public/img/logo.png) no-repeat 50% 50%"
+    // console.log(bg_wrap.scrollTop)
+    let value2 = window.innerHeight
+    console.log(value2)
+    if(bg_wrap.scrollTop < value2 * 1.54){
+        bg_wrap.style.background = "url(/img/logo.png) no-repeat 50% 50%"
     }
-    if(bg_wrap.scrollTop > 1415){
-        bg_wrap.style.backgroundImage = "url(/public/img/blockchain2.jpg)"
+    if(bg_wrap.scrollTop > value2 * 1.54){
+        bg_wrap.style.backgroundImage = "url(/img/blockchain2.jpg)"
     }
-    if(bg_wrap.scrollTop > 2830){
-        bg_wrap.style.backgroundImage = "url(/public/img/blockchain.jpg)"
+    if(bg_wrap.scrollTop > value2 * 1.54 * 2){
+        bg_wrap.style.backgroundImage = "url(/img/blockchain.jpg)"
     }
-    if(bg_wrap.scrollTop > 1415*3){
-        bg_wrap.style.backgroundImage = "url(/public/img/blockchain2.jpg)"
+    if(bg_wrap.scrollTop > value2 * 1.54 * 3){
+        bg_wrap.style.backgroundImage = "url(/img/blockchain2.jpg)"
     }
    
 })
