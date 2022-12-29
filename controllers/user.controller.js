73684515a99d2075b6ee99a6c8c5e8c49402a9d9
baek <baekbr13@gmail.com>
2022-12-29 21:49:26 +0900

    fix : 오류수정

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -36,7 +36,9 @@ exports.getInsert = (req, res, next) => {
 exports.postInsert = async (req, res, next) => {
     const { userId, userPw, userName, nickName, birth, gender, phoneNum, telNum, idx } = req.body
     const insert = await service.insert(userId, userPw, userName, nickName, birth, gender, phoneNum, telNum, req.file.filename)
-    res.render("user/welcome.html", { userId, userName, gender, phoneNum, telNum })
+    const { token } = req.cookies
+    const { image } = await service.fImage(token)
+    res.render("user/welcome.html", { userId, userName, gender, phoneNum, telNum, image })
 
 }
 
