a164c080530f8eeec07e523eed6cea38c5953974
Hanjin <csa0545@gamil.com>
2022-12-31 17:45:14 +0900

    feat : 아이디 칸이 비어있을때 확인

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,19 +1,25 @@
 let checkValue = document.getElementById("searchForm")
 let msgEle = document.getElementById("userId")
-
+let chva = 0
 
 checkValue.addEventListener("submit", function (e) {
-    if (msgEle.value.length === 0) {
-        e.preventDefault()
+    if (msgEle.value === "") {
         alert("아이디 값을 입력해주세요")
+        e.preventDefault()
         msgEle.focus()
     }
+    chva += 1
 })
 
 password.addEventListener("click", (e) => {
-    if (msgEle.value === "") {
+    if (msgEle.value === "" && chva === 0) {
+        alert("아이디 값을 입력해주세요")
         msgEle.focus()
         msgEle.classList = "itemAdd"
         items.scrollTop -= 105
+    } else if (msgEle.value === "" && chva !== 1) {
+        chva = 0
+        msgEle.focus()
+        alert("아이디 값을 입력해주세요")
     }
 })
